975db5ac68f5afbd9fb931790a39a5de4270fc04
aleph-null84 <smallest.infinite.cardinal.num@gmail.com>
2017-07-22 18:29:09 +0300

    lesson #16

--- src/com/yuriy/servlets/LoginServlet.java
+++ src/com/yuriy/servlets/LoginServlet.java
@@ -28,6 +28,15 @@ public class LoginServlet extends HttpServlet {
 		
 		LoginService loginService = new LoginService();
 		boolean result = loginService.authenticate(userId, password);
+		
+		if (result) {
+			response.sendRedirect("success.jsp");
+			return;
+		}
+		else {
+			response.sendRedirect("login.jsp");
+			return;
+		}
 	}
 
 }
